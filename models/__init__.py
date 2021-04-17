737663c7214e7e3e3c0585e522e51f9aaad030f5
Mohammed ALIF <malif78p@gmail.com>
2021-04-17 18:15:42 +0200

    First commit, added REST API to access items, stores and users authentication

--- models/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 11 14:16:40 2021
-
-@author: moham
-"""
-
-
--- resources/__init__.py
+++
@@ -1,8 +0,0 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Apr 11 14:17:08 2021
-
-@author: moham
-"""
-
-
